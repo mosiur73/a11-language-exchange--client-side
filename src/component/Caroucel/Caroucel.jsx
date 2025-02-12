aef773b3766dc758cd0048f57806f5abc59b467d
mosiur49 <bpi2021cmt44@gmail.com>
2025-02-12 11:39:55 +0600

    carosel unclickable button is fixed

--- src/component/Caroucel/Caroucel.jsx
+++ src/component/Caroucel/Caroucel.jsx
@@ -28,11 +28,11 @@ const Caroucel = () => {
     <img
       src="https://i.ibb.co.com/RjbZqM1/133746373-10291989.jpg"
       className="w-full h-[500px]" />
-    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-      <a href="#slide4" className="btn btn-circle">❮</a>
+    {/* <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      <a href="#slide4" className="btn btn-circle"></a>
      
-      <a href="#slide2" className="btn btn-circle">❯</a>
-    </div>
+      <a href="#slide2" className="btn btn-circle"></a>
+    </div> */}
   </div>
         </SwiperSlide>
         <SwiperSlide>
@@ -41,11 +41,11 @@ const Caroucel = () => {
     <img
       src="https://i.ibb.co.com/Jvgbdqt/34293277-8150319.jpg"
       className="w-full h-[500px]" />
-    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between ">
-      <a href="#slide4" className="btn btn-circle">❮</a>
+    {/* <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between ">
+      <a href="#slide4" className="btn btn-circle"></a>
      <h2>International mother language</h2>
-      <a href="#slide2" className="btn btn-circle">❯</a>
-    </div>
+      <a href="#slide2" className="btn btn-circle"></a>
+    </div> */}
   </div>
          
          
@@ -57,11 +57,11 @@ const Caroucel = () => {
     <img
       src="https://i.ibb.co.com/sVR2J4K/21830422-6469066.jpg"
       className="w-full h-[500px]" />
-    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+    {/* <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
       <a href="#slide4" className="btn btn-circle">❮</a>
       
       <a href="#slide2" className="btn btn-circle">❯</a>
-    </div>
+    </div> */}
   </div>
         </SwiperSlide>
         <SwiperSlide>
@@ -70,11 +70,11 @@ const Caroucel = () => {
     <img
       src="https://i.ibb.co.com/1zWfdBm/2564886-370427-PBG5-MV-943.jpg"
       className="w-full h-[500px]" />
-    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+    {/* <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
       <a href="#slide4" className="btn btn-circle">❮</a>
 
       <a href="#slide2" className="btn btn-circle">❯</a>
-    </div>
+    </div> */}
   </div>
         </SwiperSlide>
       </Swiper>
