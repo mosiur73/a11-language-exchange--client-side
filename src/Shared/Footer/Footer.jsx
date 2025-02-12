7fa7e4dded0ae7827c374b684abeb6c4e9384be0
mosiur49 <bpi2021cmt44@gmail.com>
2025-02-12 12:06:12 +0600

    footer is fixed

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -8,19 +8,18 @@ const Footer = () => {
     <a className="link link-hover">Branding</a>
     <a className="link link-hover">Design</a>
     <a className="link link-hover">Marketing</a>
-    <a className="link link-hover">Advertisement</a>
+    
   </nav>
   <nav>
     <h6 className="footer-title">Company</h6>
     <a className="link link-hover">About us</a>
     <a className="link link-hover">Contact</a>
     <a className="link link-hover">Jobs</a>
-    <a className="link link-hover">Press kit</a>
+    
   </nav>
   <nav>
     <h6 className="footer-title">Social</h6>
     <a className="link link-hover">Twitter</a>
-    <a className="link link-hover">Instagram</a>
     <a className="link link-hover">Facebook</a>
     <a className="link link-hover">GitHub</a>
   </nav>
